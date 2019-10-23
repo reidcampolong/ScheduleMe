3545875ba8cb095ec5c90a9e43c496b1b70cd953
Reid Campolong <reidcampolong@gmail.com>
2019-10-22 21:07:23 -0400

    Added year and class styled components

---
+++ src/modules/components/Class.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+
+interface IClassProps {
+  title: string;
+}
+const Class = ({ title }: IClassProps) => <StyledYear>{title}</StyledYear>;
+const StyledYear = styled.div`
+  border-style: solid;
+  border-color: orange;
+  border-width: 1px;
+
+  min-width: 60px;
+  min-height: 60px;
+  max-width: 60px;
+  max-height: 60px;
+`;
+
+export default Class;
--- src/modules/components/Year.tsx
+++ src/modules/components/Year.tsx
@@ -1,13 +1,32 @@
 import React, { Fragment } from 'react';
 import styled from 'styled-components';
+import Class from './Class';
 
 interface IYearProps {
   title: string;
 }
 
 const Year = ({ title }: IYearProps) => {
-  return <StyledYear>{title}</StyledYear>;
+  return (
+    <StyledYear>
+      {title}
+      <StyledRow>
+        <Class title="220" />
+        <Class title="260" />
+      </StyledRow>
+      <StyledRow>
+        <Class title="220" />
+        <Class title="260" />
+      </StyledRow>
+    </StyledYear>
+  );
 };
+
+const StyledRow = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  padding-bottom: 10px;
+`;
 
 const StyledYear = styled.div`
   border-style: solid;
@@ -15,9 +34,9 @@ const StyledYear = styled.div`
   border-width: 1px;
 
   /*Allow for flexibility*/
-  width: 200px;
+  width: 140px;
   height: 400px;
-  min-width: 100px;
+  min-width: 140px;
   min-height: 200px;
 `;
 
